fa099dba1597d672e1b7b06fbab933925e3ffb17
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-08-13 23:11:57 +0200

    refactor: handle audio in JS

--- src/api.v
+++ src/api.v
@@ -3,7 +3,6 @@ import json
 
 fn (mut app App) bind() {
 	app.window.bind('get_config', get_config, &app)
-	app.window.bind('play_sound', play_sound, &app)
 	app.window.bind('toggle_audio', toggle_audio, &app)
 	app.window.bind('open_in_browser', open_in_browser, &app)
 }
@@ -14,17 +13,8 @@ pub fn get_config(event_id &char, _ &char, app &App) {
 	app.window.result(event_id, .value, json.encode(app.config))
 }
 
-pub fn play_sound(_ &char, _ &char, app &App) {
-	if app.config.audio {
-		spawn play_wav_file()
-	}
-}
-
 pub fn toggle_audio(event_id &char, args &char, mut app App) {
 	app.config.audio = !app.config.audio
-	if app.config.audio {
-		play_sound(event_id, args, app)
-	}
 	app.window.result(event_id, .value, json.encode(app.config.audio))
 }
 
