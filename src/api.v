8b02cc6d14f74914d1ca91b1d52b22e465012007
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-08-28 23:58:57 +0200

    ✨ use `Event` API

--- src/api.v
+++ src/api.v
@@ -1,35 +1,36 @@
-import webview { EventId, JSArgs }
+import webview { Event }
 import os
 
 fn (mut app App) bind() {
-	app.window.bind('get_config', get_config, &app)
-	app.window.bind('play_audio', play_audio, &app)
-	app.window.bind('toggle_audio', toggle_audio, &app)
-	app.window.bind('open_in_browser', open_in_browser, &app)
+	app.window.bind('get_config', app.get_config) // Bind a function method to have the app struct available.
+	app.window.bind('play_audio', app.play_audio)
+	app.window.bind_ctx('toggle_audio', toggle_audio, app) // Alternatively, use the ctx ptr to pass a struct.
+	app.window.bind('open_in_browser', open_in_browser)
 }
 
-// The functions we bind do not have to be public. For semantic reasons we can do it anyway.
+// The functions we bind do not have to be public. For semantic reasons or
+// if we would want to generate docs for pub functions we can do it anyway.
 
-pub fn get_config(event_id EventId, _ JSArgs, app &App) {
-	app.window.@return(event_id, .value, app.config)
+pub fn (app &App) get_config(e &Event) {
+	e.@return(app.config)
 }
 
-pub fn play_audio(_ EventId, _ JSArgs, app &App) {
+pub fn (app &App) play_audio(_ &Event) {
 	if app.config.audio {
 		spawn play_wav_file()
 	}
 }
 
-pub fn toggle_audio(event_id EventId, args JSArgs, mut app App) {
+pub fn toggle_audio(e &Event, mut app App) {
 	app.config.audio = !app.config.audio
 	if app.config.audio {
-		play_audio(event_id, args, app)
+		app.play_audio(e)
 	}
-	app.window.@return(event_id, .value, app.config.audio)
+	e.@return(app.config.audio)
 }
 
-pub fn open_in_browser(_ EventId, args JSArgs, app &App) {
-	os.open_uri(args.string(0)) or {
+pub fn open_in_browser(e &Event) {
+	os.open_uri(e.string(0)) or {
 		eprintln(err)
 		return
 	}
