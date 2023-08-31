0fa43c72f0a204c0fc825e110f085bee23f4932b
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-08-31 03:35:09 +0200

    🐛 don't allow port 0

--- src/config.v
+++ src/config.v
@@ -8,6 +8,8 @@ mut:
 	port     int  // Default port the app is tried to be served on.
 }
 
+const default_port = 34763
+
 fn (mut app App) load_config() ! {
 	if !os.is_dir(cfg_dir) {
 		os.mkdir_all(cfg_dir)!
@@ -16,10 +18,11 @@ fn (mut app App) load_config() ! {
 		os.write_file(cfg_file, toml.encode(Config{}))!
 	}
 	user_config := toml.parse_file(cfg_file)!
+	port := user_config.value('port').default_to(default_port).int()
 	app.config = Config{
 		audio: user_config.value('audio').default_to(true).bool()
 		frequent: user_config.value('frequent').default_to(true).bool()
-		port: user_config.value('port').default_to(34763).int()
+		port: if port != 0 { port } else { default_port }
 	}
 }
 
