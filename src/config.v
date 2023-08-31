7e849c15b6a55106fd1db052527eb59c7b21b602
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-08-31 06:35:17 +0200

    🎨 improve default config handling

--- src/config.v
+++ src/config.v
@@ -3,12 +3,10 @@ import toml
 
 struct Config {
 mut:
-	audio    bool // Last state of audio feedback feedback setting for emoji selections
-	frequent bool // Controls whether or not frequently used emojis are shown
-	port     int  // Default port the app is tried to be served on.
+	audio    bool = true // Last state of audio feedback feedback setting for emoji selections
+	frequent bool = true // Controls whether or not frequently used emojis are shown
+	port     int  = 34763 // Default port the app is tried to be served on.
 }
-
-const default_port = 34763
 
 fn (mut app App) load_config() ! {
 	if !os.is_dir(cfg_dir) {
@@ -18,11 +16,10 @@ fn (mut app App) load_config() ! {
 		os.write_file(cfg_file, toml.encode(Config{}))!
 	}
 	user_config := toml.parse_file(cfg_file)!
-	port := user_config.value('port').default_to(default_port).int()
 	app.config = Config{
-		audio: user_config.value('audio').default_to(true).bool()
-		frequent: user_config.value('frequent').default_to(true).bool()
-		port: if port != 0 { port } else { default_port }
+		audio: user_config.value('audio').bool()
+		frequent: user_config.value('frequent').bool()
+		port: user_config.value('port').int()
 	}
 }
 
