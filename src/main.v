25863ba883fd0f5f5cf891029513321df6f69e6f
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-11-16 11:41:54 +0100

    ✨ use new webview serve functions

--- src/main.v
+++ src/main.v
@@ -3,12 +3,11 @@ import os
 
 @[heap]
 struct App {
-	window &Webview
 mut:
+	window Webview
 	config Config
 	cache  LocalStorage
 	port   int
-	proc   os.Process // OS process spawned when running the app with `v -d dev`
 }
 
 fn main() {
@@ -17,28 +16,28 @@ fn main() {
 	}
 	app.config.load()
 	app.cache.load()
-	app.serve()
 	os.signal_opt(.int, app.handle_interrupt)!
-	app.run()
+	app.run()!
 }
 
-fn (mut app App) run() {
+fn (mut app App) run() ! {
 	app.bind()
 	app.window.set_title('Emoji Mart')
-	app.window.set_icon(paths.icon) or {}
 	app.window.set_size(352, 435, .@none)
-	app.window.navigate('http://localhost:${app.port}')
+	app.window.set_icon(paths.icon) or {} // FIXME: `assets/emoji-mart.ico` not recognized on linux.
+	$if dev ? {
+		app.window.serve_dev(paths.ui)!
+	} $else {
+		app.window.serve_static(paths.ui)
+	}
 	app.window.run()
-	app.window.destroy()
 	app.end()
 }
 
 fn (mut app App) end() {
+	app.window.destroy()
 	app.config.save() or { panic('failed to save config. ${err}') }
 	app.cache.save() or { panic('failed to load cache. ${err}') }
-	$if dev ? {
-		app.proc.signal_pgkill()
-	}
 }
 
 fn (mut app App) handle_interrupt(signal os.Signal) {
